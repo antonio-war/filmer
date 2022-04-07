c6dd6eda596a37ba5370bf54a60a3902e065ac6f
AntonioWar <antonioguerra.it@gmail.com>
2022-04-07 10:09:13 +0200

    Initial Commit

---
+++ Filmer/FilmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmerApp.swift
+//  Filmer
+//
+//  Created by AntonioWar on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FilmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
